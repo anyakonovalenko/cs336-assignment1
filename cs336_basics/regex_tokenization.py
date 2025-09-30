b0f38dd484e68db4c0b2e6a6954562703214822a
Anna Konovalenko <annakonovlnko@gmail.com>
2025-09-30 22:01:30 +0100

    parallelization and split by special tokens

---
+++ cs336_basics/regex_tokenization.py
@@ -0,0 +1,114 @@
+import regex as re
+
+
+def get_stats(ids):
+    counts = {}
+    proceed = True
+    for i in range(len(ids) - 1):
+        key_two = (ids[i], ids[i + 1])
+        if key_two in counts:
+            counts[key_two] += 1
+        else:
+            counts[key_two] = 1
+    return counts
+
+def merge_bpe(ids, pair, idx):
+
+    newids = [] # update text in token with a new pair
+    i = 0
+    while i < len(ids) - 1:
+        if ids[i] == pair[0] and ids[i + 1] == pair[1]:
+            newids.append(idx)
+            i += 2
+        else:
+            newids.append(ids[i])
+            i += 1
+    if i == len(ids) - 1:
+        newids.append(ids[i])
+
+    return newids
+
+# def decode_my(ids, merges):
+#     print(merges.values())
+#     for i, merge in sorted(merges.items(), key = lambda x: x[1], reverse = True): # later merges depend on earlier tokens
+#         new_ids = []
+#         for id in ids:
+#             if id == merge:
+#                 new_ids.append(i[0])
+#                 new_ids.append(i[1])
+#             else:
+#                 new_ids.append(id)
+#         ids = new_ids
+#     text = bytes(ids).decode('utf-8')  # This gives you 'é'
+#     return text
+
+
+def decode_karp(ids, merges):
+    vocab = {i: bytes([i]) for i in range(256)}
+    for (m1, m2), value in merges.items(): # it will be added in order of creating
+        vocab[value] = vocab[m1] + vocab[m2] #so it will always be accassible in vocabulary, for later ids iw t will be longer
+    text_bytes = b"".join([vocab[i] for i in ids])
+    return text_bytes.decode("utf-8", errors= 'replace')  # for example 128 is a continuation byte, not the first one, so we need to do it as it can come from LLM and we will not be able to decode
+
+def encode(text, merges):
+    tokens = list(text.encode("utf-8"))
+    for (m1, m2), value in merges.items():
+        new_tokens = []
+        j = 0
+        while j < len(tokens):
+            if j < len(tokens)-1 and tokens[j] == m1 and tokens[j+1] == m2:
+                new_tokens.append(value)
+                j += 2
+            else:
+                new_tokens.append(tokens[j])
+                j += 1
+        tokens = new_tokens
+    print(tokens)
+    return tokens
+
+def trainingBPE(input_path, vocab_size, special_tokens) -> tuple[dict[int, bytes], list[tuple[bytes, bytes]]]:
+
+    PAT = re.compile( r"""'s|'t|'re|'ve|'m|'ll|'d| ?\p{L}+| ?\p{N}+| ?[^\s\p{L}\p{N}]+|\s+(?!\S)|\s+""")
+    # for match in re.finditer(pattern, text):
+    print(re.findall(PAT, "some text here.UHUH"))
+
+    #2 in text page 7 is not clear the difference between original BPE
+
+    text = "The quick é brown fox jumps over the lazy dog. This sentence contains every letter of the alphabet at least once."
+    # with open(input_path, 'r', encoding='utf-8') as f:
+    #     text = f.read()
+    tokens = text.encode("utf-8")
+    tokens = list(map(int, tokens))
+    # vocab_size = 259
+    num_merges = vocab_size - 256 - len(special_tokens)
+    ids = list(tokens)
+    merges = {} #(int, int) -> int  you had othervise previously newid -> (pair (int, int))
+    merges_list = []
+    for i in range(num_merges):
+        stats = get_stats(ids)
+        pair = max(stats, key=lambda i: stats[i])  # find pair
+        iter_num = 256 + i
+        ids = merge_bpe(ids, pair, iter_num)
+        merges[pair] = iter_num
+
+
+    vocab = {i: bytes([i]) for i in range(256)}
+    for (m1, m2), value in merges.items():  # it will be added in order of creating
+        vocab[value] = vocab[m1] + vocab[m2]
+        merges_list.append((vocab[m1], vocab[m2]))
+    for i in range(len(special_tokens)):
+        vocab[256+num_merges+i] = special_tokens[i].encode('utf-8')
+    return vocab, merges_list
+
+
+    # encode('werwer jkdljsf the quickl', merges)
+    # print(bytes([128]))
+    # print(merges)
+
+
+
+    # return re.findall(PAT, "some text here.UHUH")
+    print(vocal, merges)
+    return vocab, merges
+
+trainingBPE('ff', 500, ["<|endoftext|>"])
--- tests/adapters.py
+++ tests/adapters.py
@@ -8,7 +8,7 @@ import numpy.typing as npt
 import torch
 from jaxtyping import Bool, Float, Int
 from torch import Tensor
-from cs336_basics.basic_tokenization import trainingBPE
+from cs336_basics.parallel_tokenization import trainingBPE
 
 
 def run_linear(
