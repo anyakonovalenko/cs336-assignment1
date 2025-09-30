e79021e75b218299d0db56209e051844d0517d1d
Anna Konovalenko <annakonovlnko@gmail.com>
2025-09-30 16:39:28 +0100

    passed 2nd test, basic BPE

--- cs336_basics/basic_tokenization.py
+++ cs336_basics/basic_tokenization.py
@@ -1,9 +1,8 @@
 import regex as re
 
 
-def get_stats(ids):
-    counts = {}
-    proceed = True
+def get_stats(ids, counts=None):
+    counts = {} if counts is None else counts
     for i in range(len(ids) - 1):
         key_two = (ids[i], ids[i + 1])
         if key_two in counts:
@@ -15,16 +14,19 @@ def get_stats(ids):
 def merge_bpe(ids, pair, idx):
 
     newids = [] # update text in token with a new pair
-    i = 0
-    while i < len(ids) - 1:
-        if ids[i] == pair[0] and ids[i + 1] == pair[1]:
-            newids.append(idx)
-            i += 2
-        else:
-            newids.append(ids[i])
-            i += 1
-    if i == len(ids) - 1:
-        newids.append(ids[i])
+    for chunk_id in range(len(ids)):
+        newids_chunk = []
+        i = 0
+        while i < len(ids[chunk_id]) - 1:
+            if ids[chunk_id][i] == pair[0] and ids[chunk_id][i + 1] == pair[1]:
+                newids_chunk.append(idx)
+                i += 2
+            else:
+                newids_chunk.append(ids[chunk_id][i])
+                i += 1
+        if i == len(ids[chunk_id]) - 1:
+            newids_chunk.append(ids[chunk_id][i])
+        newids.append(newids_chunk)
 
     return newids
 
@@ -63,41 +65,48 @@ def encode(text, merges):
                 new_tokens.append(tokens[j])
                 j += 1
         tokens = new_tokens
-    print(tokens)
     return tokens
 
-def pre_tokenization():
-    PAT =  r"""'s|'t|'re|'ve|'m|'ll|'d| ?\p{L}+| ?\p{N}+| ?[^\s\p{L}\p{N}]+|\s+(?!\S)|\s+"""
+
+def trainingBPE(input_path, vocab_size, special_tokens) -> tuple[dict[int, bytes], list[tuple[bytes, bytes]]]:
+
+    PAT = re.compile( r"""'s|'t|'re|'ve|'m|'ll|'d| ?\p{L}+| ?\p{N}+| ?[^\s\p{L}\p{N}]+|\s+(?!\S)|\s+""")
     # for match in re.finditer(pattern, text):
-
-    #2 in text page 7 is not clear the difference between original BPE
-
-
     text = "The quick é brown fox jumps over the lazy dog. This sentence contains every letter of the alphabet at least once."
-    tokens = text.encode("utf-8")
-    tokens = list(map(int, tokens))
-    vocab_size = 259
-    num_merges = vocab_size - 256
-    ids = list(tokens)
+    # with open(input_path, 'r', encoding='utf-8') as f:
+    #     text = f.read()
+    text_chunks = re.findall(PAT, text)
+    num_special = len(special_tokens)
+    # ids = [[symbol for symbol in ch] for ch in text_chunks]
+    ids = [[byte_val + num_special for byte_val in list(ch.encode("utf-8"))] for ch in text_chunks]
+    # vocab_size = 259
+    num_merges = vocab_size - 256 - len(special_tokens)
     merges = {} #(int, int) -> int  you had othervise previously newid -> (pair (int, int))
-    for i in range(num_merges):
-        stats = get_stats(ids)
-        pair = max(stats, key=lambda i: stats[i])  # find pair
-        iter_num = 256 + i
+    merges_list = []
+    vocab = {}
+    for i, token in enumerate(special_tokens):
+        vocab[i] = token.encode('utf-8')
+
+    for i in range(256):
+        vocab[num_special + i] = bytes([i])
+
+    for merge_id in range(num_merges):
+        stats = {}
+        for chunk_ids in ids:
+            stats = get_stats(chunk_ids, stats)
+
+        # Use vocab to get byte representation for lexicographic comparison
+        pair = max(stats, key=lambda p: (stats[p], (vocab[p[0]]), vocab[p[1]]))
+
+        iter_num = num_special+ 256 + merge_id
+        vocab[iter_num] = vocab[pair[0]] + vocab[pair[1]]  # Add new token to vocab
         ids = merge_bpe(ids, pair, iter_num)
         merges[pair] = iter_num
-    encode('werwer jkdljsf the quickl', merges)
-    # print(bytes([128]))
-    # print(merges)
+
+    for (m1, m2), value in merges.items():
+        merges_list.append((vocab[m1], vocab[m2]))
 
 
+    return vocab, merges_list
 
-    # return re.findall(PAT, "some text here.UHUH")
-    return 0
-
-
-
-
-
-
-print(pre_tokenization())
+trainingBPE('ff', 300, ['fdf'])
--- tests/adapters.py
+++ tests/adapters.py
@@ -8,6 +8,7 @@ import numpy.typing as npt
 import torch
 from jaxtyping import Bool, Float, Int
 from torch import Tensor
+from cs336_basics.basic_tokenization import trainingBPE
 
 
 def run_linear(
@@ -589,4 +590,5 @@ def run_train_bpe(
                 representing that <token1> was merged with <token2>.
                 Merges are ordered by order of creation.
     """
-    raise NotImplementedError
+    vocab, merges = trainingBPE(input_path, vocab_size, special_tokens)
+    return vocab, merges
